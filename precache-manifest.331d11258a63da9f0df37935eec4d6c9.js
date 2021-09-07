7bb90e7d01dec56908fb0ef980402b99a96d2a83
DESKTOP-HK4VH6P\dla67 <byeonggoon>
2021-09-07 12:00:41 +0900

    Updates

---
+++ precache-manifest.331d11258a63da9f0df37935eec4d6c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7ef5878dc5df73d6db9d427ea7514c",
+    "url": "/U1L/index.html"
+  },
+  {
+    "revision": "bea63e5b8da42844e060",
+    "url": "/U1L/static/css/2.ba6db5ec.chunk.css"
+  },
+  {
+    "revision": "91a63fb351c5eb3c384b",
+    "url": "/U1L/static/css/main.773f6fdb.chunk.css"
+  },
+  {
+    "revision": "bea63e5b8da42844e060",
+    "url": "/U1L/static/js/2.b8a45cee.chunk.js"
+  },
+  {
+    "revision": "91a63fb351c5eb3c384b",
+    "url": "/U1L/static/js/main.9c5e14e9.chunk.js"
+  },
+  {
+    "revision": "c2ef9faf6954c0ada8da",
+    "url": "/U1L/static/js/runtime-main.4f089260.js"
+  },
+  {
+    "revision": "6c4033cbb08c02b1ee411649a774ebda",
+    "url": "/U1L/static/media/create-welcome1.6c4033cb.jpg"
+  },
+  {
+    "revision": "9658fd70e5bb65d0e3df1fdd258dc355",
+    "url": "/U1L/static/media/ipfs-logo.9658fd70.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/U1L/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/U1L/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/U1L/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/U1L/static/media/slick.f97e3bbf.svg"
+  }
+]);
